0e5ce657019d30c2209ef9dc52dc083f71fb7ea4
benbelgacemhichem <benbelgacemhichem@gmail.com>
2021-01-19 14:33:12 +0300

    Fix front office namespace contoller

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('front_office.welcome');
+Route::group(['namespace' => 'FrontOffice'], function() {
+    Route::get('/', 'WelcomeController@index')->name('welcome');
 });
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
